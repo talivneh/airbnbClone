60953a924e182268a98c08439b5ca0d82d2e88c2
Tal Livneh <talivneh8@gmail.com>
2021-11-08 10:39:39 +0200

    scrollbar

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({ exploreData, cardsData }) {
 
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+          <div className="hiiden-scrollbar flex space-x-3 overflow-scroll p-3 -ml-3">
             {cardsData.map(({ title, img }) => (
               <MediumCard key={img} img={img} title={title} />
             ))}
